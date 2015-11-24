9f9040f36f189dc78cdcd4b6555fb37583abfd09
Obtuse <theobtuseautodidact@gmail.com>
2015-11-24 08:49:10 -0700

    functional with color styling

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,29 +1,43 @@
 require 'sinatra'
 require 'sinatra/reloader'
-#
-# SECRET_NUMBER = rand(100)
-#
-# SECRET_NUMBER = rand(100)
+require 'pry'
 
 set :number, rand(100)
 
 def check_guess(guess)
-  guess = guess.to_i
-  if guess > settings.number && (guess - 5) > settings.number
-    "Way too high!"
-  elsif guess > settings.number
-    "Too high!"
-  elsif guess < settings.number && (guess + 5) < settings.number
-    "Way too low!"
-  elsif guess < settings.number
-     "Too low!"
-  elsif guess == settings.number
-    "You got it right! \n\n the Secret Number is #{settings.number}"
+  if guess == ""
+    "C'mon, guess a number!"
+  else
+    guess = guess.to_i
+    if guess > settings.number && (guess - 5) > settings.number
+      "Way too high!"
+    elsif guess > settings.number
+      "Too high!"
+    elsif guess < settings.number && (guess + 5) < settings.number
+      "Way too low!"
+    elsif guess < settings.number
+       "Too low!"
+    elsif guess == settings.number
+      "You got it right!"
+     end
    end
-
 end
+
+def color_response(string)
+  if string == "Way too high!" || string == "Way too low!" || string == "C'mon, guess a number!"
+    color = "red"
+  elsif string == "Too high!" || string == "Too low!"
+    color = "rgb(255, 153, 153)"
+  else
+    color = "rgb(0, 255, 0)"
+  end
+end
+
 get '/' do
   guess = params["guess"]
   message = check_guess(guess)
-  erb :index, :locals => { :message => message}
+  color = color_response(message)
+  victory = "The Secret Number is #{settings.number}" if message == "You got it right!"
+
+  erb :index, :locals => { :message => message, :color => color, :victory => victory}
 end
